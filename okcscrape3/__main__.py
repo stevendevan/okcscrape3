717c13575dddee479303df785e561c2e0c510326
stevendevan <stevendevan12@gmail.com>
2018-07-20 20:11:48 -0700

    Moved argparser stuff back into main. Begin config parse testing.

--- okcscrape3/__main__.py
+++ okcscrape3/__main__.py
@@ -5,12 +5,18 @@
 
 import csv
 import argparse
+import configparser
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-def parse_cl_args():
+def main():
+
+    # Parse config.ini
+    configs = configparser.ConfigParser()
+    configs.read('config.ini')
+
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     parser.add_argument(
@@ -23,12 +29,6 @@ def parse_cl_args():
     parser_fetch.add_argument('--cookie-file', help='File containing cookies.')
 
     args = parser.parse_args()
-    import ipdb; ipdb.set_trace()  # breakpoint 620c3c2c //
-    
-
-
-def main():
-    parse_cl_args()
 
 
 if __name__ == '__main__':
