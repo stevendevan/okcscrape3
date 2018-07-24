64e00f8612cb4d5ae24fda5158d4e4e324f88423
stevendevan <stevendevan12@gmail.com>
2018-07-23 21:46:55 -0700

    Added subfunction flag and made clargs object into a dict.

--- okcscrape3/__main__.py
+++ okcscrape3/__main__.py
@@ -22,7 +22,7 @@ def main():
     configs.read(os.path.join(dirname, 'config.ini'))
 
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='subroutine')
     parser.add_argument('--webdriver-path',
                         default=configs['global']['webdriver_path'],
                         help='Specify the path of the webdriver.')
@@ -37,7 +37,7 @@ def main():
     parser.add_argument('--no-save-configs', action='store_false',
                         default=True, dest='save_configs', help='TODO')
 
-    parser_find = subparsers.add_parser('find', help='Run find.')
+    parser_find = subparsers.add_parser('findusers', help='Run find.')
     parser_find.add_argument('--match-url-suffix',
                              default=configs['findusers']['match_url_suffix'],
                              help='TODO')
@@ -45,10 +45,10 @@ def main():
                              default=configs['findusers']['usernames_outfile'],
                              help='Name of outfile.')
     parser_find.add_argument('--num-usernames',
-                             default=configs['findusers']['nume_usernames'],
+                             default=configs['findusers']['num_usernames'],
                              help='TODO')
 
-    parser_fetch = subparsers.add_parser('fetch', help='Run fetch.')
+    parser_fetch = subparsers.add_parser('fetchusers', help='Run fetch.')
     parser_fetch.add_argument('--cookie-file', help='File containing cookies.')
     parser_fetch.add_argument('--outfile',
                               default=configs['fetchusers']['profiles_outfile'],
@@ -57,16 +57,17 @@ def main():
                               default=configs['fetchusers']['num_profiles'],
                               help='TODO')
 
-    args_obj = parser.parse_args()
+    # vars() because we need to be able to access the contents like obj[str]
+    args_obj = vars(parser.parse_args())
 
     #
 
-    if args_obj.find:
-        functions.find(args_obj)
-    elif args_obj.fetch:
-        functions.fetch(args_obj)
+    if args_obj['subroutine'] == 'findusers':
+        functions.findusers(args_obj)
+    elif args_obj['subroutine'] == 'fetchusers':
+        functions.fetchusers(args_obj)
 
-    if args_obj.save_configs:
+    if args_obj['save_configs']:
         for section in configs.sections():
             for key, value in configs[section].items():
                 configs.set(section, key, value)
