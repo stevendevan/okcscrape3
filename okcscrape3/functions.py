7f962a6d8b99c913c72cad8688cf36b4e405e6da
stevendevan <stevendevan12@gmail.com>
2018-07-24 16:01:36 -0700

    Added and tested print_config function.

--- okcscrape3/functions.py
+++ okcscrape3/functions.py
@@ -1,10 +1,20 @@
-def find(thing):
+import configparser
+
+def findusers(thing):
     print('Run find.')
     print(thing)
 
 
-def fetch(*args):
+def fetchusers(args_obj):
     print('Run fetch.')
+
+
+def print_config(configs: configparser.ConfigParser) -> None:
+    for section in configs.sections():
+        print('[{}]'.format(section))
+        for key in configs[section]:
+            print('{} = {}'.format(key, configs[section][key]))
+        print('')
 
 
 def get_webpage(browser, url):
