09facb216716cb121fde3b04ff3d5133de1374f5
stevendevan <stevendevan12@gmail.com>
2018-07-26 23:13:32 -0700

    Added findusers function 1st iteration.

--- okcscrape3/functions.py
+++ okcscrape3/functions.py
@@ -1,25 +1,50 @@
 import configparser
+import datetime
 import regex
-import os
+import time
+import os  # pathlib also an option to consider
 
 from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 import selenium
 from selenium import webdriver
 
 
 def findusers(args_obj: dict) -> None:
+    """TODO
+    """
     print('Run find.')
     homedir = os.path.dirname(__file__)
     webdriver_path = os.path.join(homedir, args_obj['webdriver_path'])
     browser = webdriver.Chrome(executable_path=webdriver_path)
 
-    url = args_obj['base_url'] + \
-        args_obj['match_url_suffix']
+    url = args_obj['base_url'] + args_obj['match_url_suffix']
 
     html = get_webpage(browser, url, args_obj)
-    usernames_list = extract_usernames_from_html(html)
-    import ipdb; ipdb.set_trace()  # breakpoint b83abd5a //
+    usernames_list = []
+    while len(usernames_list) < int(args_obj['num_usernames']):
+        usernames_list += extract_usernames_from_html(html)
+        print('{}/{} usernames found'
+              .format(len(usernames_list), args_obj['num_usernames']))
 
+    usernames_path = os.path.join(homedir, args_obj['outfile'])
+    try:
+        usernames_df = pd.read_csv(usernames_path)
+    except FileNotFoundError:
+        print('file "{}" does not exist, but it soon shall.'
+              .format(args_obj['outfile']))
+        usernames_df = pd.DataFrame()
+
+    usernames_df_new = pd.DataFrame(
+        {'profile': usernames_list,
+         'profile_fetched': np.zeros(len(usernames_list), dtype=int),
+         'date_found': [datetime.datetime.now().strftime(r'%Y/%m/%d')] *
+         len(usernames_list)})
+
+    usernames_df = pd.concat([usernames_df, usernames_df_new], axis=0)
+    usernames_df.drop_duplicates(subset='profile', keep='first', inplace=True)
+    usernames_df.to_csv(path_or_buf=usernames_path, index=False)
 
 
 def fetchusers(args_obj):
@@ -32,6 +57,9 @@ def print_config(configs: configparser.ConfigParser) -> None:
         for key in configs[section]:
             print('{} = {}'.format(key, configs[section][key]))
         print('')
+
+
+# Internal functions?
 
 
 def get_webpage(browser, url, args_obj):
