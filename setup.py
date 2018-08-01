742f437974ad45a6b02260c4916bd8b01004cbd0
stevendevan <stevendevan12@gmail.com>
2018-08-01 15:05:38 -0700

    Added package dependencies and auto package finding.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='okcscrape3',
     version='0.1.0',
-    packages=['okcscrape3'],
-    entry_points={
-        'console_scripts': [
-            'okcscrape3 = okcscrape3.__main__:main'
-        ]
-    })
+    packages=find_packages(),
+    install_requires=['beautifulsoup4', 'pandas', 'selenium'],
+    entry_points={'console_scripts': ['okcscrape3 = okcscrape3.__main__:main']
+                  },
+)
