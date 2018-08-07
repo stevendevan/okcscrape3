fb0cb9871573542cbada0a657842b247c4943b2a
stevendevan <stevendevan12@gmail.com>
2018-08-06 19:11:10 -0700

    Finished moving get_webpage to util.py

--- okcscrape3/functions.py
+++ okcscrape3/functions.py
@@ -7,7 +7,6 @@ import os  # pathlib also an option to consider
 
 from bs4 import BeautifulSoup
 import pandas as pd
-import selenium
 from selenium import webdriver
 
 from okcscrape3 import util
@@ -108,32 +107,6 @@ def print_config(configs: configparser.ConfigParser) -> None:
 
 
 # Internal functions below?
-
-
-def get_webpage(browser: selenium.webdriver.Chrome,
-                url: str,
-                max_query_attempts: int) -> str:
-    """Use selenium webdriver to fetch a webpage and return the html.
-    """
-
-    """Improvement ideas:
-    1.  Look into returning page after a set amount of time.
-        Some unnecessary elements take a long time to fully load.
-        Would likely require args in browser obj creation or in get() function.
-    """
-
-    for attempt in range(max_query_attempts):
-        try:
-            browser.get(url)  # [1]
-        except selenium.common.exceptions.TimeoutException as e:
-            if attempt < max_query_attempts:
-                continue
-            else:
-                raise
-        else:
-            break
-
-    return browser.page_source
 
 
 def extract_usernames_from_html(html: str) -> list:
