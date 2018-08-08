48becfc7c6a5d2431d476d82519ec8c525e1d391
stevendevan <stevendevan12@gmail.com>
2018-08-08 01:18:10 -0700

    Added function plan comments.

--- okcscrape3/fetchusers.py
+++ okcscrape3/fetchusers.py
@@ -1,5 +1,27 @@
+#
+
+import pandas as pd
+
 from okcscrape3 import util
 
 
 def fetchusers():
     print('Run fetchusers.')
+    """Checklist
+    1.  Read in usernames from usernames csv, get a list of ones that
+        haven't beem fetched yet.
+
+        Probably read in entire csv into pandas DataFrame, then filter
+        by 'profile_fetched' flag.
+    2.  Check if profiles csv created, if not, create it
+    3.  Load in cookies file (json?)
+    4.  Create webdriver
+    5.  Add cookies and whatever else to webdriver (verbosity levels, etc)
+    6.  Loop through username list, earliest first
+            a.  sleep
+            a.  build url
+            a.  get webpage
+            a.  send html to parser functions
+            a.  append results to the specified csv
+    7.  ??
+    """
