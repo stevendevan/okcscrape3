1fc094fe655e7e1f81bd2b1266a5c2e78515e65d
steven <stevendevan12@gmail.com>
2018-08-15 17:43:19 -0700

    Added regex dependency.

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='okcscrape3',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['beautifulsoup4', 'pandas', 'selenium'],
+    install_requires=['beautifulsoup4',
+                      'pandas',
+                      'selenium',
+                      'regex'],
     entry_points={'console_scripts': ['okcscrape3 = okcscrape3.__main__:main']
                   },
 )
